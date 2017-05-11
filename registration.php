e80fd2b3d14bcb9b97de51a13b8003f83f09d973
davron028 <davron.rakhmatullaev@gmail.com>
2017-05-12 00:14:57 +0500

    Add files via upload

--- registration.php
+++ registration.php
@@ -5,29 +5,101 @@
 	<title>Registration</title>
 	<link rel="stylesheet" href="registration-style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link href="insert.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-<?php include "checkValidation.php"; ?>	
 
+<?php 
+include ('insert.php');
+$User= new User();
+
+$fullnameErr = $emailErr = $genderErr = $passwordErr = "";
+$fullname = $email = $gender = $password = $region ="";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $region=$_POST["region"];
+
+  if (empty($_POST["fullname"])) {
+    $fullnameErr = "Name is required";
+  } else {
+    $fullname = test_input($_POST["fullname"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$fullname)) {
+      $fullnameErr = "Only letters and white space allowed"; 
+    }
+  }
+  
+  if (empty($_POST["email"])) {
+    $emailErr = "Email is required";
+  } else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $emailErr = "Invalid email format"; 
+    }
+  }
+  
+   if (empty($_POST["password"])) {
+    $passwordErr = "Password is required";
+  } else {
+    $password = test_input($_POST["password"]);
+    // check if password contains one caputal letter and sign
+    if (!preg_match("/([0-9])(?=^\w{8,20}$)(?=\w*[A-Z])^\w+$/",$password)) {
+      $passwordErr = "At most one capital letter and digit allowed"; 
+    }
+  }
+
+  if ($_POST["password"] == $_POST["confirm_password"]) {
+   $password = "Correct";
+  }
+  else {
+   $passwordErr = "You wrote two different password";
+  }
+
+  if (empty($_POST["gender"])) {
+    $genderErr = "Gender is required";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+  if($password&&$fullname&&$email&&$gender)
+  {
+
+    $result=$User->signUp($fullname,$email,$password,$region,$gender);
+    if($result){
+        header("Location: main.php");
+    } 
+  }
+  else{
+    header("Location: registration.php");
+  }
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+ ?>
 <div class="container orient">
 			<div class="row">
 			<div class="col-sm-3"></div>
 			<div class="col-sm-6 ramka">
 			<div id="qori">
-            <form class="form-horizontal" action="insert.php" role="form" method="POST">
-                <h2>Registration Form</h2>
+            <form class="form-horizontal" action="" role="form" method="POST">
+                <h2>Регистрация</h2>
                 <div class="form-group">
                     <label for="fullname" class="col-sm-3 control-label">Full Name</label>
                     <div class="col-sm-9">
                         <input type="text" id="fullname" placeholder="Full Name" class="form-control" name="fullname" autofocus>
+                        
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="email" class="col-sm-3 control-label">Email</label>
+                 
                     <div class="col-sm-9">
                         <input type="email" name="email" id="email" placeholder="Email" class="form-control">
                     </div>
@@ -67,17 +139,17 @@
                     <div class="col-sm-6">
                         <div class="row">
                             <div class="col-sm-9">
-                               <select id="gender" name="gender" class="form-control">
-		                            <option>Male</option>
-		                            <option>Female</option>
-							</select>  
+                               <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
+                              <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
+                              <span class="error">* <?php echo $genderErr;?></span>
+                              <br><br>
                             </div>
                         </div>
                     </div>
                 </div> 
                 <div class="form-group">
                     <div class="col-sm-9 col-sm-offset-3">
-                        <button type="submit" name="submit" class="btn btn-info color">Register</button>
+                        <input type="submit" name="submit" class="btn btn-info color" value="Register">
                     </div>
                 </div>
             </form>
